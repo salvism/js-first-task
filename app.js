2559dff87c4c8d7bf63f19faa607c79a263db567
Salvi Ismayilzada <salviai@code.edu.az>
2023-03-29 19:45:24 +0400

    all tasks done

--- app.js
+++ app.js
@@ -2,15 +2,37 @@ function Check() {
   var groupNo = document.getElementById("group_no").value;
   var firstNum = groupNo.slice(-3, -2);
   if (firstNum == 1) {
-    document.getElementById("yellow").innerHTML = `Group No: ${groupNo}`;
+    let h_3 = document.getElementById("text");
+    h_3.style.color = "yellow";
+    document.getElementById("text").innerHTML = `Group No: ${groupNo}`;
+  } else if (firstNum == 2) {
+    let h_3 = document.getElementById("text");
+    h_3.style.color = "red";
+    document.getElementById("text").innerHTML = `Group No: ${groupNo}`;
+  } else if (firstNum == 3) {
+    let h_3 = document.getElementById("text");
+    h_3.style.color = "black";
+    document.getElementById("text").innerHTML = `Group No: ${groupNo}`;
+  } else {
+    alert("Group No is not valid");
   }
-  else if (firstNum == 2) {
-    document.getElementById("red").innerHTML = `Group No: ${groupNo}`;
+}
+
+// TASK 1
+function myFunction() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "You pressed OK!";
+  } else {
+    text = "You canceled!";
   }
-  else if (firstNum == 3) {
-    document.getElementById("black").innerHTML = `Group No: ${groupNo}`;
-  }
-  else{
-    alert('Group No is not valid')
+  document.getElementById("demo").innerHTML = text;
+}
+
+// TASK 2
+function myFunction() {
+  let age = prompt("Yash", "18");
+  if (age < 18) {
+    document.getElementById("demo").innerHTML = "Yashiniz 18-den kicikdir";
   }
 }
